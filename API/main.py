f681e5b80daca5c0bbbc3cf329f50aae9b7dca9f
e-wach <e_wangting@hotmail.com>
2025-05-10 10:25:55 +0200

    API historical and daily endpoints

--- API/main.py
+++ API/main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 import logging
 import os
 import time
 
 from handlers.teams import get_teams
 from handlers.games import get_games, get_games_week
-from handlers.stats import get_team_metrics, get_team_season_stats, get_team_stats
+from handlers.stats import get_team_stats
 from handlers.theodds_api import get_odds_week
 
 
@@ -13,51 +13,82 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 # Env. variables defined in Terraform
-# topic_teams = os.getenv("TOPIC_nba_teams")
-# topic_games = os.getenv("TOPIC_nba_games")
-# topic_games_week = os.getenv("TOPIC_nba_games_week")
-# topic_metrics = os.getenv("TOPIC_team_metrics")
-# topic_season_stats = os.getenv("TOPIC_team_season_stats")
-# topic_stats = os.getenv("TOPIC_team_stats")
-# topic_odds = os.getenv("TOPIC_odds_week")
-
-# variables to use WITHOUT terraform
-topic_teams = "nba_teams"
-topic_games = "nba_games"
-topic_games_week = "nba_games_week"
-topic_metrics = "team_metrics"
-topic_season_stats = "team_season_stats"
-topic_stats = "team_stats"
-topic_odds = "odds_week"
+topic_teams = os.getenv("TOPIC_nba_teams", "nba_teams")
+topic_games = os.getenv("TOPIC_nba_games", "nba_games")
+topic_games_week = os.getenv("TOPIC_nba_games_week", "nba_games_week")
+topic_stats = os.getenv("TOPIC_team_stats", "team_stats")
+topic_odds = os.getenv("TOPIC_odds_week", "odds_week")
 
 
-@app.route("/fetchAll", methods=["POST"])
-def fetch_all():
+@app.route("/setup/<str:type>", methods=["POST"])
+def historical_data(type):
     try:
-        get_teams(topic_teams)
-        logging.info("NBA teams sent to PubSub")
-        time.sleep(5)
-        get_games(topic_games)
-        logging.info("NBA games sent to PubSub")
-        time.sleep(5)
-        get_games_week(topic_games_week)
-        logging.info("NBA upcoming games sent to PubSub")
-        time.sleep(5)
-        get_team_metrics(topic_metrics)
-        logging.info("NBA team metrics sent to PubSub")
-        time.sleep(5)
-        get_team_season_stats(topic_season_stats)
-        logging.info("NBA season stats sent to PubSub")
-        time.sleep(5)
-        get_team_stats(topic_stats)
-        logging.info("NBA team stats sent to PubSub")
-        time.sleep(5)
-        # get_odds_week(topic_odds)
-        # logging.info("NBA betting odds sent to PubSub")
-        return {"status": "success"}, 200
+        if type == "teams":
+            get_teams(topic_teams)
+            logging.info("NBA teams sent to PubSub")
+        elif type == "games":
+            seasons = ["2022-23", "2023-24"]
+            get_games(topic_games, seasons)
+            logging.info("NBA historical games sent to PubSub")
+        elif type == "stats": ######### FALTA EL MÉTODO NUEVO
+            seasons = ["2022-23", "2023-24"]
+            get_team_stats(topic_stats, seasons)
+            logging.info("NBA historical stats sent to PubSub")
+        elif type == "all":
+            get_teams(topic_teams)
+            logging.info("NBA teams sent to PubSub")
+            time.sleep(10)
+            seasons = ["2022-23", "2023-24"]
+            get_games(topic_games, seasons)
+            logging.info("NBA historical games sent to PubSub")
+            time.sleep(10)
+            seasons = ["2022-23", "2023-24"]
+            get_team_stats(topic_stats, seasons)
+            logging.info("NBA historical stats sent to PubSub")
+        else:
+            return jsonify({"error": "Invalid type provided."}), 400
+        return {"status": "success", "message": f"{type} data sent to PubSub"}, 200
     except Exception as e:
-        logging.error(f"Error in nba_odds_week: {str(e)}")
+        logging.error(f"Error fetching historical data for {type}: {str(e)}")
         return {"status": "error", "message": str(e)}, 500
+    
+
+@app.route("/daily/<str:type>", methods=["POST"])
+def daily_data(type):
+    try:
+        if type == "games":
+            seasons = ["2024-25"]
+            get_games(topic_games, seasons)
+            logging.info("2024-25 NBA games sent to PubSub")
+        elif type == "upcomingGames":
+            get_games_week(topic_games_week)
+            logging.info("NBA upcoming games sent to PubSub")
+        elif type == "stats": ######### FALTA EL MÉTODO NUEVO
+            seasons = ["2024-25"]
+            get_team_stats(topic_stats, seasons)
+            logging.info("2024-25 NBA stats sent to PubSub")
+        # elif type == "odds":
+            # get_odds_week(topic_odds)
+            # logging.info("NBA betting odds sent to PubSub")
+        elif type == "all":
+            seasons = ["2024-25"]
+            get_games(topic_games, seasons)
+            logging.info("2024-25 NBA games sent to PubSub")
+            time.sleep(10)
+            get_games_week(topic_games_week)
+            logging.info("NBA upcoming games sent to PubSub")
+            seasons = ["2024-25"]
+            get_team_stats(topic_stats, seasons)
+            logging.info("2024-25 NBA stats sent to PubSub")
+            # get_odds_week(topic_odds)
+            # logging.info("NBA betting odds sent to PubSub")
+        else:
+            return jsonify({"error": "Invalid type provided."}), 400
+        return {"status": "success", "message": f"{type} data sent to PubSub"}, 200
+    except Exception as e:
+        logging.error(f"Error fetching daily data for {type}: {str(e)}")
+        return {"status": "error", "message": str(e)}, 500
+
 
 
 if __name__ == "__main__":
