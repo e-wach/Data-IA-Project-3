4cb128559f55673e80f569f841cbc9d421ee76f1
Claudiarg13 <cromerog13@gmail.com>
2025-05-14 13:38:29 +0200

    Cloud_function_years
    Cambiar de ndjson a csv. Y configurar la cloud function para triggear desde un bucket

--- cloudfunctions/past_games/main.py
+++ cloudfunctions/past_games/main.py
@@ -3,25 +3,24 @@ import logging
 from google.cloud import bigquery
 import functions_framework
 
-PROJECT_ID = os.getenv("GCP_PROJECT", "western-mix-459018-g4")
+PROJECT_ID = os.getenv("GCP_PROJECT", "feisty-lamp-442712-m8")
 DATASET_ID = os.getenv("BQ_DATASET", "nba_dataset")
 NBA_GAMES_TABLE = os.getenv("BQ_TABLE", "all_games")
 
 bq = bigquery.Client(project=PROJECT_ID)
 
 logging.basicConfig(
-    level    = logging.INFO,
-    format   = "%(asctime)s %(levelname)s %(message)s",
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
 )
 
 @functions_framework.cloud_event
 def callback_games(cloud_event):
-
     bucket_name = cloud_event.data.get("bucket")
     object_name = cloud_event.data.get("name")
 
-    if not object_name or not object_name.endswith(".ndjson"):
-        logging.info(f"Ignoring non-ndjson file: {object_name}")
+    if not object_name or not object_name.endswith(".csv"):
+        logging.info(f"Ignoring non-csv file: {object_name}")
         return
 
     uri = f"gs://{bucket_name}/{object_name}"
@@ -30,15 +29,21 @@ def callback_games(cloud_event):
     logging.info(f"Starting load from {uri} into {table_ref}")
 
     job_config = bigquery.LoadJobConfig(
-        source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+        source_format=bigquery.SourceFormat.CSV,
+        skip_leading_rows=1,
         autodetect=True,
-        time_partitioning=bigquery.TimePartitioning(field="GAME_DATE"),
+        write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
+        range_partitioning=bigquery.RangePartitioning(
+            field="TEAM_ID",
+            range_=bigquery.PartitionRange(start=1610612737, end=1610612767, interval=1),
+        ),
     )
 
-    load_job = bq.load_table_from_uri(uri, table_ref, job_config=job_config)
     try:
-        load_job.result()  
+        load_job = bq.load_table_from_uri(uri, table_ref, job_config=job_config)
+        load_job.result()
         logging.info(f"Loaded {load_job.output_rows} rows into {table_ref}")
     except Exception as e:
         logging.error(f"BigQuery load_job failed: {e}")
         raise
+
