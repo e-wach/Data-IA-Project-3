47eaea84454ca7350cf181d99f5350c26ef2ce86
e-wach <e_wangting@hotmail.com>
2025-05-18 00:16:24 +0200

    cf upcoming games

--- terraform/modules/cloudfunctions/main.tf
+++ terraform/modules/cloudfunctions/main.tf
@@ -1,5 +1,4 @@
-# Cloud Function para leer de pub/sub y cargar en BigQuery
-
+# Cloud Function from PubSub to BigQuery (nba_games) for most recent games (API SPORTS DATA)
 resource "google_storage_bucket_object" "function_zip" {
   name   = "games.zip"
   bucket = var.bucket_name
@@ -50,4 +49,53 @@ resource "google_cloudfunctions2_function_iam_member" "unauthenticated_invoker" 
   role           = "roles/cloudfunctions.invoker"
   member         = "allUsers"
   depends_on = [google_cloudfunctions2_function.callback_games]
+}
+
+
+# Cloud Functions from PubSub to CloudSQL (nba_games_week)
+resource "google_storage_bucket_object" "function_zip_upcoming" {
+  name   = "games.zip"
+  bucket = var.bucket_name
+  source = "${path.module}/zip/upcoming_games.zip"
+}
+resource "google_cloudfunctions2_function" "upcoming_games" {
+  name        = "upcominggamestosql"
+  location    = var.region
+  project     = var.project_id
+
+  build_config {
+    runtime     = "python312"
+    entry_point = "callback_upcoming_games"
+    source {
+      storage_source {
+        bucket = var.bucket_name
+        object = google_storage_bucket_object.function_zip_upcoming.name
+      }
+    }
+  }
+  service_config {
+    available_memory   = "256M"
+    timeout_seconds    = 60
+    environment_variables = {
+      PROJECT_ID = var.project_id
+      SQL_HOST   = var.sql_host
+      SQL_USER   = var.sql_user
+      SQL_PASS   = var.sql_pass
+      SQL_DB     = var.sql_db
+    }
+  }
+  event_trigger {
+    event_type    = "google.cloud.pubsub.topic.v1.messagePublished"
+    pubsub_topic  = "projects/${var.project_id}/topics/${var.topic_names[1]}"
+    trigger_region = var.region
+  }
+}
+
+resource "google_cloudfunctions2_function_iam_member" "unauthenticated_invoker_upcoming" {
+  project        = var.project_id
+  location       = var.region
+  cloud_function = google_cloudfunctions2_function.upcoming_games.name
+  role           = "roles/cloudfunctions.invoker"
+  member         = "allUsers"
+  depends_on = [google_cloudfunctions2_function.upcoming_games]
 }
--- terraform/modules/cloudfunctions/vars.tf
+++ terraform/modules/cloudfunctions/vars.tf
@@ -21,3 +21,19 @@ variable "topic_names" {
 variable "bq_table" {
   type = string
 }
+
+variable "sql_host" {
+    type = string
+}
+
+variable "sql_user" {
+    type = string
+}
+
+variable "sql_pass" {
+    type = string
+}
+
+variable "sql_db" {
+    type = string
+}
